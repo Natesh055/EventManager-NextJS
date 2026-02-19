37e5ce354c712216bf218e53874e089f5412d760
Natesh055 <singhnatesh50@gmail.com>
2026-02-19 10:46:11 +0530

    Vercel fix in mongodb.ts file, error handling added.

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,26 +1,42 @@
-
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI
+
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error("Please define the MongoDB URI");
+  throw new Error("Please define MONGODB_URI in .env.local");
 }
 
-let cached = (global as any).mongoose;
+type MongooseCache = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
 
-if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
+
+declare global {
+  var mongooseCache: MongooseCache | undefined;
 }
+
+
+const cached: MongooseCache =
+  global.mongooseCache ?? {
+    conn: null,
+    promise: null,
+  };
+
+global.mongooseCache = cached;
+
 
 export async function connectDB() {
   if (cached.conn) {
     return cached.conn;
   }
 
+
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-      return mongoose;
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: "eventsdb", // optional but recommended
+      bufferCommands: false,
     });
   }
 
